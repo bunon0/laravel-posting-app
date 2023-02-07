bbc466dbbf59a76ae32cb0777a2e3bd7bbbe3f95
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-07 17:31:06 +0900

    BaseStyleの読み込み

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>投稿編集</title>
+  @vite(['resources/js/app.js'])
 </head>
 
 <body>
