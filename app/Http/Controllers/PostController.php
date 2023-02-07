9c333ba0f30667f2282a7a93146bf65a45351c1f
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-07 12:51:40 +0900

    投稿作成のコントローラの作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -10,5 +11,22 @@ class PostController extends Controller
   public function index()
   {
     return view('posts.index');
+  }
+
+  // 作成ページ
+  public function create()
+  {
+    return view('posts.create');
+  }
+
+  // 作成機能
+  public function store(Request $request)
+  {
+    $post = new Post();
+    $post->title = $request->input('title');
+    $post->content = $request->input('content');
+    $post->save();
+
+    return redirect()->route('posts.index')->with('flash_message', '投稿が完了しました。');
   }
 }
